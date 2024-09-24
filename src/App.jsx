d2545eea59a91de717007e50b453452bfcee50f5
FridjyDesfontaines <125769705+FridjyDesfontaines@users.noreply.github.com>
2024-09-24 17:11:06 +0200

    mise en place du projet

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,20 @@ function App() {
 
   return (
     <>
-<h1 className="bg-red-400">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</h1>
-
+      <div className="h-screen bg bg-slate-900">
+        <div className="max-w-4xl mx-auto pt-20 px-6">
+          <h1 className="text-3xl text-slate-100 mb-4">la to do liste</h1>
+            <form className="mb-10">
+              <label htmlFor="todo-item"
+              className="text-slate-50">Ajouter une chose a faire</label>
+              <input type="text" className="mt-1 block w-full rounded" />
+              <button className="mt-4 py-2 px-2 bg-slate-50 rounded min-w-[115px]">Ajouter</button>
+            </form>
+            <ul>
+               
+            </ul>
+        </div>
+      </div>
     </>
   )
 }
